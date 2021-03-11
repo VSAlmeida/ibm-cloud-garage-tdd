9952d6382b8b21a9c7a4579bba5a94d73cfc6a9c
VSAlmeida <vsalmeida@ibm.com>
2021-03-11 11:53:33 -0300

    stack kata complete

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,4 +1,5 @@
-const stackFactory = () => {
+const stackFactory = (capacity = 2) => {
+  if (capacity < 1) throw new Error('invalid capacity');
   let elements = [];
   return {
     isEmpty: () => elements.length === 0,
@@ -83,5 +84,9 @@ describe('a stack', () => {
     expect(stack.pop()).toBe('1');
   });
 
-  it.todo('accepts only a positive capacity');
+  it('accepts only a positive capacity', () => {
+    expect(() => {
+      stack = stackFactory(-1);
+    }).toThrowError('invalid capacity');
+  });
 });
